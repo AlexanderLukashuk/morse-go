70a0337c5bb151a34d7218dcade42c965ae5c52d
AlexanderLukashuk <upiter465@gmail.com>
2025-06-05 20:44:06 +0500

    add languages choosing

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-var morseCode = map[string]string{
+var morseCodeRus = map[string]string{
 	// Russian
 	"А": ".-", "Б": "-...", "В": ".--", "Г": "--.", "Д": "-..", "Е": ".",
 	"Ж": "...-", "З": "--..", "И": "..", "Й": ".---", "К": "-.-", "Л": ".-..",
@@ -16,6 +16,18 @@ var morseCode = map[string]string{
 	"Ш": "----", "Щ": "--.-", "Ъ": "--.--", "Ы": "-.--", "Ь": "-..-",
 	"Э": "..-..", "Ю": "..--", "Я": ".-.-",
 
+	// Numbers
+	"0": "-----", "1": ".----", "2": "..---", "3": "...--", "4": "....-",
+	"5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.",
+
+	// Special symbols
+	".": ".-.-.-", ",": "--..--", "?": "..--..", "'": ".----.", "!": "-.-.--",
+	"/": "-..-.", "(": "-.--.", ")": "-.--.-", "&": ".-...", ":": "---...",
+	";": "-.-.-.", "=": "-...-", "+": ".-.-.", "-": "-....-", "_": "..--.-",
+	"\"": ".-..-.", "$": "...-..-", "@": ".--.-.", "¿": "..-.-", "¡": "--...-",
+}
+
+var morseCodeEng = map[string]string{
 	// English
 	"A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.",
 	"G": "--.", "H": "....", "I": "..", "J": ".---", "K": "-.-", "L": ".-..",
@@ -35,13 +47,28 @@ var morseCode = map[string]string{
 }
 
 var reverseMorseCode = map[string]string{}
+var language string
 
 func main() {
 	var input string
 	var output string
 
-	for key, value := range morseCode {
-		reverseMorseCode[value] = key
+	fmt.Println("1) English")
+	fmt.Println("2) Russian")
+	fmt.Print("Choose language: ")
+	fmt.Scanln(&language)
+
+	if language == "1" {
+		for key, value := range morseCodeEng {
+			reverseMorseCode[value] = key
+		}
+	} else if language == "2" {
+		for key, value := range morseCodeRus {
+			reverseMorseCode[value] = key
+		}
+	} else {
+		fmt.Println("WRONG INPUT")
+		return
 	}
 
 	reader := bufio.NewReader(os.Stdin)
@@ -51,8 +78,8 @@ func main() {
 
 	input = strings.ToUpper(input)
 
-	// output = coder(input)
-	output = decoder(input)
+	output = coder(input)
+	// output = decoder(input)
 
 	fmt.Println(output)
 }
@@ -65,9 +92,16 @@ func coder(str string) string {
 	for _, word := range words {
 		runes := []rune(word)
 
-		for _, rune := range runes {
-			element, _ := morseCode[string(rune)]
-			result += element + " "
+		if language == "1" {
+			for _, rune := range runes {
+				element, _ := morseCodeEng[string(rune)]
+				result += element + " "
+			}
+		} else {
+			for _, rune := range runes {
+				element, _ := morseCodeRus[string(rune)]
+				result += element + " "
+			}
 		}
 	}
 
